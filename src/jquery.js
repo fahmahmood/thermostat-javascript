ec4c1c228cc6a1a649bb3ddc9241f89bd885e939
Faisal Mahmood <fahmahmood@gmail.com>
2016-06-23 17:10:15 +0100

    about to design on CSS

--- src/jquery.js
+++ src/jquery.js
@@ -1,7 +1,7 @@
 $ (document).ready(function(){
   var thermostat = new Thermostat();
 
-  $('#temperature').text(thermostat.getCurrentTemperature()+'°C');
+  $('#temperature').text(thermostat.getCurrentTemperature());
   $('#increase-temp').click(function(){
     thermostat.increaseTemperature();
     updatesTemp();
@@ -28,7 +28,7 @@ $ (document).ready(function(){
   })
 
 function updatesTemp(){
-  $('#temperature').text(thermostat.getCurrentTemperature()+'°C')
+  $('#temperature').text(thermostat.getCurrentTemperature())
   if(thermostat.energyUsage() === 'low-usage') {
     $('#temperature').css('color', 'green')
   } else if(thermostat.energyUsage() === 'medium-usage') {
@@ -42,5 +42,35 @@ function updatesTemp(){
   function updatesPSM(OnOff){
     $('#power-saving-status').text(OnOff)
   };
+
+  // $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  //   $('#current-temperature').text(data.main.temp);
+  // })
+
+//   $('#current-city').change(function() {
+//   var city = $('#current-city').val();
+//   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+//     $('#current-temperature').text(data.main.temp)
+//   })
+// })
+
+
+function displayWeather(city) {
+   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+   var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+   var units = '&units=metric';
+   $.get(url + token + units, function(data) {
+     $('#current-temperature').text(data.main.temp);
+   })
+ }
+
+ displayWeather('London');
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+})
+
 
 });
